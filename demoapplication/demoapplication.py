3bdb6bbade3567361d6586bc23d7e3770b2db10a
Alex Corvin <acorvin@redhat.com>
2018-05-31 12:50:01 -0400

    Add logging to the demo app

--- demoapplication/demoapplication.py
+++ demoapplication/demoapplication.py
@@ -1,12 +1,23 @@
 #! /usr/bin/env python3
 
+import logging
+import sys
+
 from flask import Flask
 
 app = Flask(__name__)
+log = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(('%(asctime)s - %(name)s - '
+                               '%(levelname)s - %(message)s'))
+log.setFormatter(formatter)
+log.setLevel(logging.INFO)
+app.logger.addHandler(log)
+app.logger.setLevel(logging.INFO)
 
 
 @app.route('/')
 def hello():
+    app.logger.info('Got a request')
     return 'Hello, world!'
 
 
