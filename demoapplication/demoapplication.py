a7cd9c4e509b66ccbec06378350f4c453ede3d38
Alex Corvin <acorvin@redhat.com>
2018-07-20 13:09:50 -0400

    Updates

--- demoapplication/demoapplication.py
+++ demoapplication/demoapplication.py
@@ -3,7 +3,10 @@
 import logging
 import sys
 
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import generate_latest, Counter
+
+requests_count = Counter('requests_count', 'The number of requests')
 
 app = Flask(__name__)
 log = logging.StreamHandler(sys.stdout)
@@ -18,7 +21,13 @@ app.logger.setLevel(logging.INFO)
 @app.route('/')
 def hello():
     app.logger.info('Got a request')
+    requests_count.inc()
     return 'Hello, world!'
+
+
+@app.route('/metrics/')
+def metrics():
+    return Response(generate_latest())
 
 
 if __name__ == '__main__':
